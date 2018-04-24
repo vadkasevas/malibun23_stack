62c2633f44013d28a3b689a5db0b2aed13e18864
vadkasevas <v.tikkoyev@mail.ru>
2018-04-24 17:12:31 +0300

    esCode

--- tests/schemas.js
+++ tests/schemas.js
@@ -1,61 +1,59 @@
 import { assert } from 'meteor/practicalmeteor:chai';
 
-if(Meteor.isServer) {
-        describe('Schemas', () => {
-            beforeEach(() => {});
-            afterEach(() => {});
 
-            it(`esCode Невалидный код test ^ 7 & dwd;`, function(done){
-                var schema = new SimpleSchema({
-                    code:Schemas.esCode({label:'JS код'})
-                });
-                try {
-                    schema.validate({
-                        code:'test dwd'
-                    });
-                }catch(e){
-                    console.log(e);
-                    return done();
-                }
-                return done(new Error('Код должен быть невалидным'));
-            });
+describe('Schemas', () => {
+    beforeEach(() => {});
+    afterEach(() => {});
 
-            it(`esCode Валидный код this.test;`, function(done){
-                var schema = new SimpleSchema({
-                    code:Schemas.esCode({label:'JS код'})
-                });
-                try {
-                    schema.validate({
-                        code:'this.test;'
-                    });
-                    return done();
-                }catch(e){
-                    return done(e);
-                }
-            });
-
-            it(`esCode custom`, function(done){
-                var schemaMessage = function(msg){
-                    var key = md5(msg);
-                    SimpleSchema.messages({
-                        [key]:msg
-                    });
-                    return key;
-                };
-
-                var schema = new SimpleSchema({
-                    code:Schemas.esCode({label:'JS код',custom(){
-                        return schemaMessage('Custom error');
-                        }})
-                });
-                try {
-                    schema.validate({
-                        code:'this.test;'
-                    });
-                    return done(new Error('Custom error not working'));
-                }catch(e){
-                    return done();
-                }
-            });
+    it(`esCode Невалидный код test ^ 7 & dwd;`, function(done){
+        var schema = new SimpleSchema({
+            code:Schemas.esCode({label:'JS код'})
         });
-}
+        try {
+            schema.validate({
+                code:'test dwd'
+            });
+        }catch(e){
+            return done();
+        }
+        return done(new Error('Код должен быть невалидным'));
+    });
+
+    it(`esCode Валидный код this.test;`, function(done){
+        var schema = new SimpleSchema({
+            code:Schemas.esCode({label:'JS код'})
+        });
+        try {
+            schema.validate({
+                code:'this.test;'
+            });
+            return done();
+        }catch(e){
+            return done(e);
+        }
+    });
+
+    it(`esCode custom`, function(done){
+        var schemaMessage = function(msg){
+            var key = md5(msg);
+            SimpleSchema.messages({
+                [key]:msg
+            });
+            return key;
+        };
+
+        var schema = new SimpleSchema({
+            code:Schemas.esCode({label:'JS код',custom(){
+                return schemaMessage('Custom error');
+                }})
+        });
+        try {
+            schema.validate({
+                code:'this.test;'
+            });
+            return done(new Error('Custom error not working'));
+        }catch(e){
+            return done();
+        }
+    });
+});
